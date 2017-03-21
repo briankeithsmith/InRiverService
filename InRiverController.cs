4da17f418f8ae2c63ca2ee1d697221c75a0791ca
briankeithsmith <brian@1barsystems.com>
2017-03-20 21:33:28 -0400

    Added InRiverController example for pushing items to queue

---
+++ InRiverController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading.Tasks;
+using APIModels.InRiver;
+using System.Web.Http.Cors;
+
+namespace ServicesAPI.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "get,post")]
+    public class InRiverController : BaseController
+    {
+        [HttpGet]
+        [Route("InRiver/Queue/{CompanyID}/{Item}")]
+        public async Task<IHttpActionResult> PushToQueue(string CompanyID, string Item)
+        {
+            long ID=await InRiverProcessor.PushItemForUpdate(CompanyID, Item);
+            return Ok(ID);
+        }
+    }
+}
