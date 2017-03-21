3cd9e751de8d408b008581391f054031156217f2
briankeithsmith <brian@1barsystems.com>
2017-03-20 21:35:34 -0400

    Added DialController example for making outbound dial through Avaya phone system

---
+++ DialController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using APIModels.Avaya;
+namespace ServicesAPI.Controllers
+{
+    public class DialController : ApiController
+    {
+        [HttpGet]
+        [Route("Dial/{extoruser}/{dest}")]
+        public IHttpActionResult Dial(string extoruser, string dest)
+        {
+            
+            return Ok(Avaya.MakeCall(extoruser, dest));
+        }
+    }
+}
